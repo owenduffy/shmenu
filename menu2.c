3bdd2a505c6017b6886e47de8c3d7113c4601bd8
Owen Duffy <owen@owenduffy.net>
1996-06-12 07:52:00 +1000

    Reformatted.

--- menu2.c
+++ menu2.c
@@ -1,4 +1,3 @@
-
 /*f-*/
 /************************************************************************
 menu: a shell script enhancer for menus
@@ -6,9 +5,12 @@ menu: a shell script enhancer for menus
 $Id$
 
 $Log$
-Revision 1.4  1996/06/11 21:47:11  owen
-Revised default/timeout handling.
+Revision 1.5  1996/06/11 21:52:00  owen
+Reformatted.
 
+ * Revision 1.4  1996/06/11  21:47:11  owen
+ * Revised default/timeout handling.
+ *
  * Revision 1.3  1996/06/11  11:19:45  owen
  * Error checking of parms.
  *
@@ -22,7 +24,6 @@ Revised default/timeout handling.
 #include <string.h>
 #include <termio.h>
 #include <unistd.h>
-
 char version[6]="1.01",*optarg,command='\0';
 char rcsid[]="$Id$";
 int optind,optopt,debug=0,timeout=0;
@@ -30,81 +31,81 @@ int rc,parmindx,selection=0,first_time,erase=0;
 char option,file_name[256]="menu.scr",options[256]="";
 struct termio sioio,sioold;
 /************************************************************************/
+
 int clear_screen()
 {
-printf("\n\n\n\n\n\n\n\n\n\n\n\n\n");
-printf("\n\n\n\n\n\n\n\n\n\n\n\n\n");
-return 0;
+  printf("\n\n\n\n\n\n\n\n\n\n\n\n\n");
+  printf("\n\n\n\n\n\n\n\n\n\n\n\n\n");
+  return 0;
 }
 /**********************************************************************/
 
 display_file(char *file_name)
 {
-char menu[260];
-FILE *fmenu;
+  char menu[260];
+  FILE *fmenu;
 
-if(fmenu=fopen(file_name,"r")){
-  while(fgets(menu,sizeof(menu)-1,fmenu))
-    fputs(menu,stdout);
-  fclose(fmenu);
+  if(fmenu=fopen(file_name,"r")){
+    while(fgets(menu,sizeof(menu)-1,fmenu))
+      fputs(menu,stdout);
+    fclose(fmenu);
   }
-else{
-  if(file_name[0]){
-    printf("ERROR: cant find menu file: %s\n",file_name);
-  if(first_time)
-    printf("Valid options are: {%s}",options);
+  else{
+    if(file_name[0]){
+      printf("ERROR: cant find menu file: %s\n",file_name);
+      if(first_time)
+        printf("Valid options are: {%s}",options);
     }
   }
-return 0;
+  return 0;
 }
 /**********************************************************************/
 
 int prompt()
 {
-char *p;
+  char *p;
 
-display_file(file_name);
-if(!first_time)
-  printf("ERROR: option is not valid, choose one of {%s}",options);
-
-printf("\nEnter option ===> ");
-if(command)
-  printf("(%c) ",command);
-
-if(ioctl(1,TCGETA,&sioold)==-1){
-  printf("ERROR: in call to ioctl() to save old tty setup\n");
-  return -1;
+  display_file(file_name);
+  if(!first_time)
+    printf("ERROR: option is not valid, choose one of {%s}",options);
+  printf("\nEnter option ===> ");
+  if(command)
+    printf("(%c) ",command);
+  if(ioctl(1,TCGETA,&sioold)==-1){
+    printf("ERROR: in call to ioctl() to save old tty setup\n");
+    return -1;
   }
-if(ioctl(1,TCGETA,&sioio)==-1){
-  printf("ERROR: in call to ioctl() to copy tty setup\n");
-  return -1;
+  if(ioctl(1,TCGETA,&sioio)==-1){
+    printf("ERROR: in call to ioctl() to copy tty setup\n");
+    return -1;
   }
-sioio.c_lflag=0;
-sioio.c_cc[VMIN]=timeout?0:1;
-sioio.c_cc[VTIME]=10;
-ioctl(1,TCSETA,&sioio);
-do{
-  option=getchar();
-  if(timeout){
-    --timeout;
-    if(!timeout)
-      option=command;
+  sioio.c_lflag=0;
+  sioio.c_cc[VMIN]=timeout?0:1;
+  sioio.c_cc[VTIME]=10;
+  ioctl(1,TCSETA,&sioio);
+  do{
+    option=getchar();
+    if(timeout){
+      --timeout;
+      if(!timeout)
+        option=command;
     }
-  }while(option<0);
-ioctl(1,TCSETA,&sioold);
-timeout=0;
-first_time=0;
-if(option==0x1b){
-  putchar('\n');
-  return -1;
   }
-if(command&&option==0x0a)
-  option=command;
-printf("%c \n",option);
-if(p=strchr(options,option))
-  return p-options+1;
-else
-  return 0;
+  while(option<0);
+  ioctl(1,TCSETA,&sioold);
+  timeout=0;
+  first_time=0;
+  if(option==0x1b){
+    putchar('\n');
+    return -1;
+  }
+  if(command&&option==0x0a)
+    option=command;
+  printf("%c \n",option);
+  if(p=strchr(options,option))
+    return p-options+1;
+  else
+    return 0;
 }
 /**********************************************************************/
 
@@ -125,6 +126,7 @@ main(int argc,char **argv)
   char opt;
 
   /* process command line options */
+
   optind=optind?optind:1;
   /*opterr=0;*/
   while(1){
@@ -162,31 +164,29 @@ main(int argc,char **argv)
     printf("No options.\n");
     return help();
   }
-
   first_time=1;
   selection=0;
   strcpy(options,argv[optind]);
   if(!strchr(options,command)){
     printf("Error: default command not valid.\n\n");
     return -1;
-    }
+  }
   if(timeout&&!command)
     timeout=0;
   if(argc>optind+1)
     strcpy(file_name,argv[optind+1]);
-
   do{
     selection=prompt();
     if(selection==-1){
       selection=0;
       break;
-      }
-    }while(!selection);
-    if(erase)
-      clear_screen();
-    else
-      printf("\n");
-    return selection;
+    }
+  }
+  while(!selection);
+  if(erase)
+    clear_screen();
+  else
+    printf("\n");
+  return selection;
 }
 /**********************************************************************/
-
