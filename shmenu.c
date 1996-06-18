a01e9a35014d9fa89bf6972b525077100f59c753
Owen Duffy <owen@owenduffy.net>
1996-06-19 08:39:55 +1000

    Merged of curses and non-curses code.

--- shmenu.c
+++ shmenu.c
@@ -5,12 +5,17 @@ menu: a shell script enhancer for menus
 $Id$
 
 $Log$
-Revision 1.12.1.2  1996/06/17 21:33:28  owen
-Update version.
+Revision 1.15  1996/06/18 22:39:55  owen
+Merged of curses and non-curses code.
 
- * Revision 1.12.1.1  1996/06/17  21:28:06  owen
- * Branch of non-curses menu. Minor revisions for consistency with curses
- * version.
+ * Revision 1.14  1996/06/16  23:16:00  owen
+ * Revision of error returns, help.
+ *
+ * Revision 1.13  1996/06/15  22:31:47  owen
+ * Revision to use curses, menufile now mandatory.
+ *
+ * Revision 1.12  1996/06/13  23:08:28  owen
+ * Further revision of rc, selectable prompt, upcase revised.
  *
  * Revision 1.12  1996/06/13  23:08:28  owen
  * Further revision of rc, selectable prompt, upcase revised.
@@ -53,59 +58,125 @@ Update version.
 #include <string.h>
 #include <termio.h>
 #include <ctype.h>
-#define ESC_EXIT 0
+#ifdef CURSES
+#include <curses.h>
+#endif
 #define NOOPT_NOEXIT -1
-#define NOOPT_EXIT 255
 #define HELP_EXIT 254
 #define ERROR_EXIT 253
-char version[6]="1.07",file_name[256]="";
+char version[6]="2.01",file_name[256]="",work[256];
 char rcsid[]="$Id$";
-int debug=0,timeout=0,option;
-int rc,parmindx,selection=0,first_time=1,erase=0,upcase=0,lrc=0;
+int debug=0,time_out=0,option,errorrow,promptrow,promptcol;
+int rc,parmindx,selection=0,pass=0,upcase=0,basic=1;
 char options[256]="",command='\0',promptstr[80]="Enter option ===>";
 struct termio sioio,sioold;
+int (*prompt)(void),(*clrscr)(void);
+/************************************************************************/
+#ifdef CURSES
+
+display_file_c(char *file_name)
+{
+  char menu[260];
+  FILE *fmenu;
+  int y=0;
+
+  clear();
+  move(0,0);
+  if(fmenu=fopen(file_name,"r")){
+    while(fgets(menu,sizeof(menu)-1,fmenu))
+      addstr(menu);
+    fclose(fmenu);
+  }
+  else{
+    printw("\nWARNING: cannot find menu file: %s\n\n",file_name);
+    printw("Valid options are: {%s}",options+1);
+  }
+  printw("\n%s ",promptstr);
+  if(command)
+    printw("(%c) ",command);
+  getyx(stdscr,promptrow,promptcol);
+  errorrow=promptrow-1;
+  return 0;
+}
+/**********************************************************************/
+
+int prompt_c()
+{
+  char *p;
+  int i;
+
+  if(pass==0)
+    display_file_c(file_name);
+  else if(pass==1){
+    move(errorrow,0);
+    printw("ERROR: option is not valid, choose one of {%s}",options+1);
+    move(promptrow,promptcol);
+  }
+  refresh();
+  do{
+    option=getch();
+    if(upcase)
+      option=toupper(option);
+    if(time_out){
+      --time_out;
+      if(!time_out)
+        option=command;
+    }
+  }
+  while(option<0);
+  if(command&&option==0x0a)
+    option=command;
+  if(p=strchr(options,option)){
+    clear();
+    refresh();
+    endwin();
+    return p-options;
+  }
+  time_out=0;
+  ++pass;
+  mvaddch(promptrow,promptcol,iscntrl(option)?'.':option);
+  move(promptrow,promptcol);
+  return NOOPT_NOEXIT;
+}
+#endif
 /************************************************************************/
 
-int clear_screen()
+int clear_b()
 {
   printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
   return 0;
 }
 /**********************************************************************/
 
-display_file(char *file_name)
+display_file_b(char *file_name)
 {
   char menu[260];
   FILE *fmenu;
 
-  if(file_name[0]){
-    clear_screen();
-    if(fmenu=fopen(file_name,"r")){
-      while(fgets(menu,sizeof(menu)-1,fmenu))
-        fputs(menu,stdout);
-      fclose(fmenu);
-    }
-    else{
-      printf("ERROR: cant find menu file: %s\n",file_name);
-      if(first_time)
-        printf("Valid options are: {%s}",options);
-    }
+  clear_b();
+  if(fmenu=fopen(file_name,"r")){
+    while(fgets(menu,sizeof(menu)-1,fmenu))
+      fputs(menu,stdout);
+    fclose(fmenu);
   }
+  else{
+    printf("ERROR: cant find menu file: %s\n",file_name);
+    printf("Valid options are: {%s}",options+1);
+  }
+  if(pass>0)
+    printf("ERROR: option is not valid, choose one of {%s}",options+1);
+  printf("\n%s ",promptstr);
+  if(command)
+    printf("(%c) ",command);
   return 0;
 }
 /**********************************************************************/
 
-
-int prompt()
+int prompt_b()
 {
   char *p;
 
-  display_file(file_name);
-  if(!first_time)
-    printf("ERROR: option is not valid, choose one of {%s}",options);
-  printf("\n%s ",promptstr);
-  if(command)
-    printf("(%c) ",command);
+  display_file_b(file_name);
   if(ioctl(1,TCGETA,&sioold)==-1){
     printf("ERROR: in call to ioctl() to save old tty setup\n");
     return ERROR_EXIT;
@@ -115,34 +186,31 @@ int prompt()
     return ERROR_EXIT;
   }
   sioio.c_lflag=0;
-  sioio.c_cc[VMIN]=timeout?0:1;
+  sioio.c_cc[VMIN]=time_out?0:1;
   sioio.c_cc[VTIME]=10;
   ioctl(1,TCSETA,&sioio);
   do{
     option=getchar();
     if(upcase)
       option=toupper(option);
-    if(timeout){
-      --timeout;
-      if(!timeout)
+    if(time_out){
+      --time_out;
+      if(!time_out)
         option=command;
     }
   }
   while(option<0);
   ioctl(1,TCSETA,&sioold);
-  timeout=0;
-  first_time=0;
-  if(option==0x1b){
-    putchar('\n');
-    return ESC_EXIT;
-  }
   if(command&&option==0x0a)
     option=command;
+  if(p=strchr(options,option)){
+    clear_b();
+    return p-options;
+  }
+  time_out=0;
+  ++pass;
   printf("%c \n",option);
-  if(p=strchr(options,option))
-    return p-options+1;
-  else
-    return file_name[0]?NOOPT_NOEXIT:NOOPT_EXIT;
+  return NOOPT_NOEXIT;
 }
 /**********************************************************************/
 
@@ -150,7 +218,7 @@ int help()
 {
   printf("\nmenu: V%s %s\n\n",version,rcsid);
   printf("Usage: menu [-c <default> [-t <timeout>]] ");
-  printf("[-d <level>] [-h] [-p <prompt>] [-r <rc>] options [<menufile>]\n\n");
+  printf("[-d <level>] [-h] [-p <prompt>] <options> <menufile>\n\n");
   printf("Copyright: Owen Duffy & Associates Pty Ltd 1987,1996.\n");
   printf("All rights reserved.\n\n");
   return HELP_EXIT;
@@ -165,13 +233,20 @@ main(int argc,char **argv)
   int opt;
   char *p;
 
+  #ifdef CURSES
+    basic=0;
+  #endif
+
   /* process command line options */
   optind=optind?optind:1;
   /*opterr=0;*/
-  while((opt=getopt(argc,argv,"c:d:hp:r:t:u"))!=EOF){
+  while((opt=getopt(argc,argv,"bc:dhp:t:u"))!=EOF){
     switch(opt){
       case 'c':
         command=optarg[0];
+        break;
+      case 'b':
+        basic=1;
         break;
       case 'd':
         if(optarg)
@@ -181,24 +256,19 @@ main(int argc,char **argv)
         if(optarg)
           strcpy(promptstr,optarg);
         break;
-      case 'r':
-        if(optarg)
-          lrc=atoi(optarg);
-        if(lrc==NOOPT_EXIT)
-          first_time=0;
-        break;
       case 'h':
         return help();
       case 't':
         if(optarg)
-          timeout=atoi(optarg);
+          time_out=atoi(optarg);
         break;
       case 'u':
         upcase=1;
         break;
       case '?':
       case ':':
-        return help();
+        help();
+        return ERROR_EXIT;
     }
   }
 
@@ -207,11 +277,12 @@ main(int argc,char **argv)
   }
   /* process non-option arg */
   if(argc<=optind){
-    printf("Error: No options supplied.\n");
+    printf("ERROR: No options.\n");
     help();
     return ERROR_EXIT;
   }
-  strcpy(options,argv[optind]);
+  options[0]=0x1b;
+  strcpy(options+1,argv[optind]);
   if(upcase){
     command=toupper(command);
     for(p=options;*p;p++)
@@ -221,19 +292,37 @@ main(int argc,char **argv)
     printf("Error: default command not valid.\n\n");
     return ERROR_EXIT;
   }
-  if(timeout&&!command)
-    timeout=0;
+  if(time_out&&!command)
+    time_out=0;
   if(argc>optind+1)
     strcpy(file_name,argv[optind+1]);
-  
+  else{
+    printf("ERROR: No menu filename.\n");
+    help();
+    return ERROR_EXIT;
+  }
+  prompt=prompt_b;
+  #ifdef CURSES
+  if(!basic){
+    if(initscr()==0)
+      basic=1;
+    else{
+      scrollok(stdscr,1);
+      noecho();
+      if(time_out)
+        halfdelay(10);
+      else 
+        raw();
+      prompt=prompt_c;
+    }
+  }
+  #endif
+
   do{
-    selection=prompt();
+      selection=prompt();
   }
   while(selection==NOOPT_NOEXIT);
-  if(selection!=ERROR_EXIT)
-    clear_screen();
-  else
-    printf("\n\n");
+ 
   return selection;
 }
 /**********************************************************************/
